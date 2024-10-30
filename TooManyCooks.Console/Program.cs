a893d2b319d8690819ff87134d63b38c5f2d0658
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2024-10-29 19:13:34 -0700

    Update Project References

---
+++ TooManyCooks.Console/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+await CustomTask.Task.Delay(TimeSpan.FromSeconds(2));
+
+Console.WriteLine("It Worked!");
